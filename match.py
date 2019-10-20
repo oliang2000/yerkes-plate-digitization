ab0ab3a083705a1c660e92461d8bedbfebc87180
Olina <53060462+oliang2000@users.noreply.github.com>
2019-10-20 08:05:25 -0500

    better

--- match.py
+++ match.py
@@ -4,12 +4,10 @@ import seaborn as sns
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
-import statsmodels.api as sm
 import utility
 
 #basic info of csv file from APT
-file = 'ca397_600'
+file = 'config4_r'
 filename = file+'.csv'
 df_apt = pd.read_csv(filename) 
 df_apt = df_apt[['CentroidRA', 'CentroidDec','Magnitude', 'MagUncertainty']]
@@ -18,35 +16,29 @@ max_ra = df_apt['CentroidRA'].max()
 min_dec = df_apt['CentroidDec'].min()
 max_dec = df_apt['CentroidDec'].max()
 print("\n------------IMAGE INFORMATION------------")
-print("ra:" ,[min_ra, max_ra])
-print("dec:", [min_dec, max_dec])
+print("ra:" ,[min_ra, max_ra], "\n", "dec:", [min_dec, max_dec])
+print("------------------------------------------")
 
-#df_apt = df_apt.rename(columns={"CentroidRA": "ra", "CentroidDec": "dec"})
-
-
-#if utility.run_next_step("Proceed fetching data from GAIA"):
-#	df_gaia = utility.get_gaia_data (min_ra, max_ra, min_dec, max_dec, 15)
-#	gaia_name = file+"_gaia.csv"
-#	export_csv = df_gaia.to_csv (gaia_name, index = None, header=True)
-#sns.regplot('ra', 'dec', data=df_gaia, fit_reg = False)
-#sns.regplot('CentroidRA', 'CentroidDec', data=df_apt, fit_reg = False).set_title("Position match")
-#fig.savefig(file+'_figs.png')
-
-##a working oneeee
-#df_gaia = pd.read_csv("ca397_600_gaia.csv") 
-#sns_plot = sns.regplot('ra', 'dec', data=df_gaia, fit_reg = False)
-#fig = sns_plot.get_figure()
-#fig.savefig(file+'_figs.png')
-
-#anorther working one
-#df_gaia = pd.read_csv("ca397_600_gaia.csv") 
-#plt.scatter(df_apt["CentroidRA"], df_apt["CentroidDec"])
-#plt.scatter(df_gaia["ra"], df_gaia["dec"])
-#plt.savefig(file+'_figs.png')
-
-df_gaia = pd.read_csv("ca397_600_gaia.csv") 
-plt = utility.p_scatter(df_gaia, df_apt, "ra", "CentroidRA")
-plt.savefig(file+'_figs.png')
+#Matching
+rp = utility.run_next_step("Fetch data from GAIA")
+if rp:
+    df_gaia = utility.get_gaia_data (min_ra, max_ra, min_dec, max_dec, 15)
+    gaia_name = file+"_gaia.csv"
+    export_csv = df_gaia.to_csv (gaia_name, index = None, header=True) 
+    plt.scatter(df_apt["CentroidRA"], df_apt["CentroidDec"], alpha = 0.4, s = 3)
+    plt.scatter(df_gaia["ra"], df_gaia["dec"], alpha = 0.4, s = 3)
+    plt.savefig(file+'_figs.png')
+    plt.xlabel("RA")
+    plt.ylabel("Dec")
+    plt.legend(bbox_to_anchor=(0, 1), loc='lower left', ncol = 2, fontsize='small')
+    title = plt.suptitle("Plate #"+file, fontsize=12)
+    plt.savefig(file+'_match.png')
+elif not rp:
+    df_gaia = pd.read_csv(file+'_gaia.csv')
+if utility.run_next_step("Proceed matching"):
+    df = utility.match_two_tables(df_gaia, df_apt, file)
+    df_filtered = df.query('diff<' + input("Cut difference at? "))
+    utility.p_scatter(file, df, df_filtered, 'phot_bp_mean_mag', 'Magnitude', lr2 = True)
 
 
 
--- utility.py
+++ utility.py
@@ -1,5 +1,10 @@
 from astroquery.gaia import Gaia
 import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
 
 def get_gaia_data (min_ra, max_ra, min_dec, max_dec, brightness):
     '''
@@ -14,7 +19,7 @@ def get_gaia_data (min_ra, max_ra, min_dec, max_dec, brightness):
     query = "SELECT ALL gaia_source.source_id,gaia_source.ra,gaia_source.dec,\
     gaia_source.pmra,gaia_source.pmdec,gaia_source.phot_bp_mean_mag FROM gaiadr2.gaia_source WHERE CONTAINS\
     (POINT('ICRS',gaiadr2.gaia_source.ra,gaiadr2.gaia_source.dec),BOX('ICRS'," + \
-    str((min_ra + max_ra)/2) + "," + str(min_dec+max_dec/2) + "," + \
+    str((min_ra + max_ra)/2) + "," + str((min_dec + max_dec)/2) + "," + \
     str(max_ra - min_ra) + "," + str(max_dec - min_dec) + "))=1 AND \
     (gaiadr2.gaia_source.phot_bp_mean_mag<=" + str(brightness) + ")" 
     job = Gaia.launch_job_async(query)
@@ -25,7 +30,7 @@ def run_next_step(question):
     '''
     Ask whether user would like to proceed to next step.
     '''
-    inpt = question + " (y/n)?"
+    inpt = question + " (y/n)? "
     g = input(inpt)
     if g == "y":
         return True
@@ -35,18 +40,19 @@ def run_next_step(question):
         print("Please enter y/n.")
         return run_next_step(question)
         
-def p_scatter(df1, df2, x, y, xlim =[0,0] , ylim=[0,0], lr1 = False, lr2 = False):
+def p_scatter(file, df1, df2, x, y, xlim =[0,0] , ylim=[0,0], lr1 = False, lr2 = False):
     '''
     This function uses matplotlib.pyplot to graph two functions and fix axis (calculated if not given).
     df2 should be the smaller subset.
     '''
+    plt.clf()
     axes = plt.gca()
-    plt.scatter(df1[x], df1[y])
-    plt.scatter(df2[x], df2[y])
+    plt.scatter(df1[x], df1[y], alpha = 0.4, s = 4)
+    plt.scatter(df2[x], df2[y], alpha = 0.4, s = 4)
     if xlim == [0,0]:
-        xlim = [df[x].min()-(df[x].max()-df[x].min())*0.05, df[x].max() + (df[x].max()-df[x].min())*0.05]
+        xlim = [df1[x].min()-(df1[x].max()-df1[x].min())*0.05, df1[x].max() + (df1[x].max()-df1[x].min())*0.05]
     if ylim == [0,0]:
-        ylim = [df[y].min()-(df[y].max()-df[y].min())*0.05, df[y].max() + (df[y].max()-df[y].min())*0.05]
+        ylim = [df1[y].min()-(df1[y].max()-df1[y].min())*0.05, df1[y].max() + (df1[y].max()-df1[y].min())*0.05]
     axes.set_xlim(xlim)
     axes.set_ylim(ylim)
     plt.xlabel(x)
@@ -59,22 +65,30 @@ def p_scatter(df1, df2, x, y, xlim =[0,0] , ylim=[0,0], lr1 = False, lr2 = False
         model = sm.OLS(df2[y], sm.add_constant(df2[x])).fit()
         pred = df2[x]*model.params[x]+model.params['const']
         plt.plot(df2[x],pred,'#fd7f28')
+    title = plt.suptitle(file + ': ' + x + ' vs. ' + y, fontsize=12)
+    plt.savefig(file + '_' + x + '_' + y + '.png')
 
-def match_two_tables(df1, df2):
+def match_two_tables(gaia, apt, file):
     df = pd.DataFrame(columns=['ra','dec','phot_bp_mean_mag','pmra','pmdec',\
                                'CentroidRA','CentroidDec','Magnitude','diff'])
-    dist = scipy.spatial.distance.cdist(df_apt[['CentroidRA', 'CentroidDec']], df_gaia[['ra', 'dec']])
+    dist = scipy.spatial.distance.cdist(apt[['CentroidRA', 'CentroidDec']], gaia[['ra', 'dec']])
     min_dist = np.argmin(dist, axis=1)
 
     m = 0
-    while m < len(df_apt):
+    while m < len(apt):
         n = min_dist[m]
-        df = df.append({'ra': df_gaia['ra'][n], 'dec': df_gaia['dec'][n],\
-                        'phot_bp_mean_mag': df_gaia['phot_bp_mean_mag'][n],\
-                        'pmra': df_gaia['pmra'][n], 'pmdec': df_gaia['pmdec'][n],\
-                        'CentroidRA': df_apt['CentroidRA'][m],'CentroidDec': df_apt['CentroidDec'][m],\
-                        'Magnitude': df_apt['Magnitude'][m],\
+        df = df.append({'ra': gaia['ra'][n], 'dec': gaia['dec'][n],\
+                        'phot_bp_mean_mag': gaia['phot_bp_mean_mag'][n],\
+                        'pmra': gaia['pmra'][n], 'pmdec': gaia['pmdec'][n],\
+                        'CentroidRA': apt['CentroidRA'][m],'CentroidDec': apt['CentroidDec'][m],\
+                        'Magnitude': apt['Magnitude'][m],\
                         'diff': dist[m][n]
                        }, ignore_index=True)
         m+=1
-    sns.distplot(df['diff']).set_title('Distribution of difference in match')
+    df['Δra'] = df.apply(lambda row: row.ra - row.CentroidRA, axis = 1)
+    df['Δdec'] = df.apply(lambda row: row.dec - row.CentroidDec, axis = 1)
+    df['Δmag'] = df.apply(lambda row: row.Magnitude - row.phot_bp_mean_mag, axis = 1)
+    sns_plot = sns.distplot(df['diff']).set_title('Distribution of difference in match')
+    fig = sns_plot.get_figure()
+    fig.savefig(file+'_hist.png')
+    return df
